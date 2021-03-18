9de392316e6da05f79a0aa41c90e892120e3fdb2
Dan Oneata <dan.oneata@gmail.com>
2021-03-18 16:06:43 +0200

    WIP Obama dataset

---
+++ scripts/download_obama.sh
@@ -0,0 +1,13 @@
+n_jobs=12
+
+base_url="https://www.youtube.com/watch?v="
+base_path="data/obama"
+
+# The filelist video-names.txt is obtained from the following repository:
+# https://github.com/supasorn/synthesizing_obama_network_training/tree/master/obama_data
+filelist="${base_path}/filelists/video-names.txt"
+video_dir="${base_path}/videos"
+
+mkdir -p ${video_dir}
+
+parallel --bar -a $filelist -j $n_jobs youtube-dl ${base_url}{} -o "${video_dir}/%\(id\)s.%\(ext\)s"
