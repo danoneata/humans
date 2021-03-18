38906aac36133a4d0f92aa17c13eb7e97be94433
Dan Oneata <dan.oneata@gmail.com>
2021-03-18 17:50:15 +0200

    MINOR Comment

--- scripts/download_obama.sh
+++ scripts/download_obama.sh
@@ -10,4 +10,5 @@ video_dir="${base_path}/videos"
 
 mkdir -p ${video_dir}
 
+# If there are problems with some videos use `-f best` option for youtube-dl
 parallel --bar -a $filelist -j $n_jobs youtube-dl ${base_url}{} -o "${video_dir}/%\(id\)s.%\(ext\)s"
