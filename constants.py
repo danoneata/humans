414442383cfc12b30d947e0f07cc83c7aafd20d2
Dan Oneata <dan.oneata@gmail.com>
2021-03-23 21:19:27 +0200

    FIX Swap left and right

--- constants.py
+++ constants.py
@@ -1,11 +1,11 @@
 LANDMARKS_INDICES = {
     "face": (0, 17),
-    "eyebrow-r": (17, 22),
-    "eyebrow-l": (22, 27),
+    "eyebrow-l": (17, 22),
+    "eyebrow-r": (22, 27),
     "nose": (27, 31),
     "nostril": (31, 36),
-    "eye-r": (36, 42),
-    "eye-l": (42, 48),
+    "eye-l": (36, 42),
+    "eye-r": (42, 48),
     "lips": (48, 60),
     "teeth": (60, 68),
 }
