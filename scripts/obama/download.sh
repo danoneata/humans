4bd230c9697dc509acdd953341dd597a3a12ac72
Dan Oneata <dan.oneata@gmail.com>
2021-03-23 07:59:15 +0200

    Download subtitles

--- scripts/obama/download.sh
+++ scripts/obama/download.sh
@@ -7,7 +7,11 @@ base_path="data/obama"
 # https://github.com/supasorn/synthesizing_obama_network_training/tree/master/obama_data
 filelist="${base_path}/filelists/video-names.txt"
 video_dir="${base_path}/videos"
+subtitles_dir="${base_path}/subtitles"
 
 mkdir -p ${video_dir}
 
 parallel --bar -a $filelist -j $n_jobs youtube-dl ${base_url}{} -f best -o "${video_dir}/%\(id\)s.%\(ext\)s"
+# Separate command for historical reasons—I've ran the download in two separate
+# steps—but the two can certainly be combined.
+parallel --bar -a $filelist -j $n_jobs youtube-dl ${base_url}{} --write-sub --skip-download -o "${subtitles_dir}/%\(id\)s.%\(ext\)s"
