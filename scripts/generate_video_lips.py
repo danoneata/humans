c7dc4f4ff0f91e7ae4264bdb19f0a7a7fc9ce7fc
Dan Oneata <dan.oneata@gmail.com>
2021-06-17 19:27:36 +0300

    ENH More plots

--- scripts/generate_video_lips.py
+++ scripts/generate_video_lips.py
@@ -69,8 +69,152 @@ def generate_true_vs_pred_multi(dataset, split, key, methods, to_overwrite=False
     landmarks_true_rec = pca.inverse_transform(pca.transform(landmarks_true_org))
     landmarks_true_org = landmarks_true_org.reshape(-1, LEN_LIPS, 2)
     landmarks_true_rec = landmarks_true_rec.reshape(-1, LEN_LIPS, 2)
+    true = landmarks_true_rec
 
-    landmarks_pred_sm = np.load(get_path_pred(dataset, key, model="asr-finetune-all-ave"))
+    def load_landmarks(path_pred):
+        landmarks_pred_sm = np.load(path_pred)
+        landmarks_pred_sm = landmarks_pred_sm.squeeze(0)
+        landmarks_pred_lg = pca.inverse_transform(landmarks_pred_sm)
+        landmarks_pred_lg = landmarks_pred_lg.reshape(-1, LEN_LIPS, 2)
+        return landmarks_pred_lg
+
+    tmp_dir = os.path.join("/tmp", key)
+    os.makedirs(tmp_dir, exist_ok=True)
+
+    num_frames = len(true)
+    num_methods = len(methods)
+    preds = {method: load_landmarks(get_path_pred(dataset, key, method, split)) for method in methods}
+
+    for i in range(num_frames):
+        fig, axs = plt.subplots(nrows=1, ncols=num_methods, figsize=(8.5, 2))
+
+        for j, method in enumerate(methods):
+            draw_lips(axs[j], true[i], dict(marker="o", color="blue"))
+            draw_lips(axs[j], preds[method][i], dict(marker="o", color="orange"))
+            if titles is None:
+                axs[j].set_title(method)
+            else:
+                axs[j].set_title(titles[j])
+
+        for ax in axs:
+            ax.set_xlim(-2.5, 2.5)
+            ax.set_ylim(-2.0, 2.0)
+            ax.set_axis_off()
+
+        path = os.path.join(tmp_dir, f"{i:05d}.png")
+
+        plt.tight_layout()
+        plt.savefig(path)
+        plt.close(fig)
+
+    make_folder(video_path)
+    subprocess.run(
+        [
+            "ffmpeg",
+            "-r",
+            "29.97",
+            "-i",
+            os.path.join(tmp_dir, "%05d.png"),
+            "-i",
+            get_path_audio(dataset, key),
+            "-y",
+            "-c:v",
+            "libx264",
+            # "-vf", "fps=29.97",
+            "-pix_fmt",
+            "yuv420p",
+            "-c:a",
+            "aac",
+            "-shortest",
+            video_path,
+        ]
+    )
+
+    shutil.rmtree(tmp_dir)
+    return video_path
+
+
+def generate_true(dataset, split, key, to_overwrite=False):
+    video_path = os.path.join("output", dataset.name ,"lips-predicted-baseline", "true", key + ".mp4")
+
+    if not to_overwrite and os.path.exists(video_path):
+        return video_path
+
+    pca = load_pca()
+
+    landmarks_true_org = np.load(get_path_true(dataset, key))
+    landmarks_true_rec = pca.inverse_transform(pca.transform(landmarks_true_org))
+    landmarks_true_org = landmarks_true_org.reshape(-1, LEN_LIPS, 2)
+    landmarks_true_rec = landmarks_true_rec.reshape(-1, LEN_LIPS, 2)
+
+    tmp_dir = os.path.join("/tmp", key)
+    os.makedirs(tmp_dir, exist_ok=True)
+
+    for i, (true_org, true_rec) in enumerate(
+        tqdm(zip(landmarks_true_org, landmarks_true_rec))
+    ):
+        fig, axs = plt.subplots(nrows=1, ncols=2, figsize=(4.50, 2.00))
+
+        draw_lips(axs[0], true_org, dict(marker="o", color="blue"))
+        draw_lips(axs[1], true_rec, dict(marker="o", color="blue"))
+
+        for ax in axs:
+            ax.set_xlim(-2.5, 2.5)
+            ax.set_ylim(-2.0, 2.0)
+            ax.set_axis_off()
+
+        axs[0].set_title("true")
+        axs[1].set_title("true (from pca)")
+
+        path = os.path.join(tmp_dir, f"{i:05d}.png")
+
+        plt.tight_layout()
+        plt.savefig(path)
+        plt.close(fig)
+
+    make_folder(video_path)
+    subprocess.run(
+        [
+            "ffmpeg",
+            "-r",
+            "29.97",
+            "-i",
+            os.path.join(tmp_dir, "%05d.png"),
+            "-i",
+            get_path_audio(dataset, key),
+            "-y",
+            "-c:v",
+            "libx264",
+            # "-vf", "fps=29.97",
+            "-pix_fmt",
+            "yuv420p",
+            "-c:a",
+            "aac",
+            "-shortest",
+            video_path,
+        ]
+    )
+
+    shutil.rmtree(tmp_dir)
+    return video_path
+
+
+def generate_pred_vs_true(dataset, split, key, method, to_overwrite=False, pca=None):
+    suffix = "" if not pca else ("pca-" + pca.name)
+    video_path = os.path.join("output", dataset.name , "lips-predicted-baseline", "pred-vs-true" + suffix, key + ".mp4")
+
+    if not to_overwrite and os.path.exists(video_path):
+        return video_path
+
+    if pca is None:
+        pca = load_pca()
+
+    landmarks_true_org = np.load(get_path_true(dataset, key))
+    landmarks_true_rec = pca.inverse_transform(pca.transform(landmarks_true_org))
+    landmarks_true_org = landmarks_true_org.reshape(-1, LEN_LIPS, 2)
+    landmarks_true_rec = landmarks_true_rec.reshape(-1, LEN_LIPS, 2)
+
+    landmarks_pred_sm = np.load(get_path_pred(dataset, key, method, split))
     landmarks_pred_sm = landmarks_pred_sm.squeeze(0)
     landmarks_pred_lg = pca.inverse_transform(landmarks_pred_sm)
     landmarks_pred_lg = landmarks_pred_lg.reshape(-1, LEN_LIPS, 2)
