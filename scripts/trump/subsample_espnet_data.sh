a4758a1344bb4c0cebe71d29d4fa0ffcd9e6cd14
Dan Oneata <dan.oneata@gmail.com>
2021-06-17 19:29:49 +0300

    ENH Iterate also over seeds

--- scripts/trump/subsample_espnet_data.sh
+++ scripts/trump/subsample_espnet_data.sh
@@ -1,3 +1,14 @@
-for n in 30 60 120 240; do
-    python scripts/subsample_espnet_data.py -d trump-chunks-cpac -n $n
+# Example:
+# bash scripts/trump/subsample_espnet_data.sh trump-chunks-manual-shots
+dset=$1
+
+if [ -z $dset ]; then
+    echo "ERR: Dataset is not specified"
+    exit 1
+fi
+
+for s in 0 1 2 3 4; do
+    for n in 30 60 120 240; do
+        python scripts/subsample_espnet_data.py -d $dset -n $n --seed $s
+    done
 done
